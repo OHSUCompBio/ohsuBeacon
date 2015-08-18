920b804dc59d2224de8d01bda73e480b2fb81e39
jacmarjorie <jacmarjorie@gmail.com>
2015-08-18 12:58:50 -0700

    updates from deployment on genomicsbeacon VM

--- server.R
+++ server.R
@@ -3,32 +3,34 @@ library(httr)
 library(rjson)
 
 shinyServer(
-  function(input, output) {
-    
-    chrom <- reactive({
-      input$chromosome
-    })
-    
-    pos <- reactive({
-      input$position
-    })
-    
-    a <- reactive({
-      input$allele
-    })
-    
-    
-    
-    output$text1 <- renderText({
-      request <- paste0("http://localhost/cgi-bin/ucscBeacon/query?chromosome=",chrom(),"&position=",pos(),"&alternateBases=",a())
-      r <- GET(request)
-      response <- fromJSON(content(r, as = "text"))
-      exists <- response$response$exists
-      if (exists){
-        paste0('Yes')
-      }else{
-        paste0('No')
-      }
-    })
-  }
+    function(input, output) {
+        
+        chrom <- reactive({
+            input$chromosome
+        })
+        
+        pos <- reactive({
+            input$position
+        })
+        
+        a <- reactive({
+            input$allele
+        })
+        
+        
+        output$text1 <- renderText({
+            request <- paste0("http://genomicsbeacon.ohsu.edu:8002/cgi-bin/ucscBeacon/query?chromosome=",chrom(),"&position=",pos(),"&alternateBases=",a())
+#            request <- paste0("http://localhost/cgi-bin/ucscBeacon/query?chromosome=",chrom(),"&position=",pos(),"&alternateBases=",a())
+            r <- GET(request)
+            response <- fromJSON(content(r, as = "text"))
+            exists <- response$response$exists
+            if (exists){
+                #paste0(response)
+		paste0('Yes. ', response[[2]]$alternateBases, ' recorded at this position.')
+            }else{
+		#paste0(response[[2]]$alternateBases)
+                paste0('No. ', response[[2]]$alternateBases, ' not recorded at this position.')
+            }
+        })
+    }
 )
--- ui.R
+++ ui.R
@@ -1,40 +1,40 @@
 library(shiny)
 
 shinyUI(fluidPage(
-  
-  id = "page",
-  
-  theme = "bootstrap.css",
-                  
-  includeCSS('response.css'),
-  titlePanel("", windowTitle="OHSU Beacon"),
-  headerPanel(
-    a(hr(img(id="logo", src="OHSU_H_RGB_pos.jpg")))
-  ),
-  
-  
-  sidebarLayout(
-    sidebarPanel(
-      helpText("Query the OHSU Beacon."),
-      textInput("chromosome", "Chromosome:", "1"),
-      textInput("position", "Position:", "1"),
-      textInput("allele", "Allele:", "T"),
-      submitButton("Submit")
+    
+    id = "page",
+    
+    theme = "bootstrap.css",
+    
+    includeCSS('response.css'),
+    titlePanel("", windowTitle="OHSU Beacon"),
+    headerPanel(
+        a(hr(img(id="logo", src="OHSU_H_RGB_pos.jpg")))
     ),
     
-    mainPanel(
-      tags$div(
-        id = "response",
-        p('Beacon responded with: '),
-        textOutput("text1")
-      )
-   )
-  ),
-
-  tags$div(
-    id = "image",
-    hr(p('Learn more about Beacons at:',
-      a('http://ga4gh.org/#/beacon')),
-    p(em('* Research with this data may have regulatory requirements; please check with research administrators at your institution.')))
-  )
+    
+    sidebarLayout(
+        sidebarPanel(
+            helpText("Query the OHSU Beacon."),
+            textInput("chromosome", "Chromosome:", "1"),
+            textInput("position", "Position:", "1234"),
+            textInput("allele", "Allele:", "T"),
+            submitButton("Submit")
+        ),
+        
+        mainPanel(
+            tags$div(
+                id = "response",
+                p('Beacon responded with: '),
+                textOutput("text1")
+            )
+        )
+    ),
+    
+    tags$div(
+        id = "image",
+        hr(p('Example: Chromosome 1, Position 10150, Allele A.'), p('Learn more about Beacons at:',
+             a('http://ga4gh.org/#/beacon')),
+           p(em('* Research with this data may have regulatory requirements; please check with research administrators at your institution.')))
+    )
 ))
